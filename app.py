fc9119ec5eca971ac214305a6d199219c38e17ce
SYED MUHAMMAD RIHAAN <rihaan@SYEDs-MacBook-Pro.local>
2025-01-30 21:37:13 +0530

    finished spotipy project

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, session, url_for, redirect, render_template
+from flask import Flask, request, session, url_for, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
+import yt_dlp
 
 
 app = Flask(__name__)
 
-clientid = "7dba3f601e93442fa2384ef0ea07890d"
-clientSecret = "7bec6b66bf644b5eb53527d062ecb1e8"
+clientid = "74382212368f478a9b8cd2028a43b4e5"
+clientSecret = "a10611d0e4524e42a06869db76dafbbf"
 
 app.secret_key = "OAHDBFiooqwbfBDEWCB"
 app.config["SESSION_COOKIE_NAME"] = "my cookie"
@@ -35,17 +36,23 @@ def getTracks():
         print("user not logged in")
         redirect(url_for("login", _external = False))
     sp = spotipy.Spotify(auth = token_info['access_token'])
-    playlists = sp.current_user_playlists()  
-    for item in playlists['items']:
-        if item['name'] == "Test playlist":
-            print(item['id'])
-    results = sp.playlist_tracks("4XryJ3s5WLGpWiwKYVeCyX")
+    playlists = sp.current_user_playlists()
+    results = sp.playlist_tracks("0hK9vlm9gBRt29abivQICT")
+    song_names = []
     for item in results['items']:
         track = item['track']
-        with open("song_names.txt", "a") as f:
-            f.write(f"{track['name']}\n")
-
+        song_names.append(f"{track['name']}\n")
     
+    with open("song_links.txt", "a") as f: 
+       for song in song_names:
+            query = f"ytsearch:{song}"
+            with yt_dlp.YoutubeDL({"quiet": True}) as ydl:
+                info = ydl.extract_info(query, download= False)
+                if "entries" in info and info['entries']:
+                    first_result = info["entries"][0]
+                    f.write(f"{first_result['webpage_url']}\n")
+
+        
 
     return "success"
 
